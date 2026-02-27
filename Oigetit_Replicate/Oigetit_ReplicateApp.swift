4b3445f605cb96402fee3d7c75fd6f27164c262e
Zaidaan <59156187+Zaidaan@users.noreply.github.com>
2026-02-28 02:31:52 +0700

    Initial Commit

---
+++ Oigetit_Replicate/Oigetit_ReplicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oigetit_ReplicateApp.swift
+//  Oigetit_Replicate
+//
+//  Created by M Zaidaan Nugroho on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Oigetit_ReplicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
