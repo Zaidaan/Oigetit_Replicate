f0cb5db86ca2a775401b6d7cf0a7a438e0ff746d
Zaidaan <59156187+Zaidaan@users.noreply.github.com>
2026-03-15 08:33:59 +0700

    feat: add UserDefault for modal preference

---
+++ Oigetit_Replicate/Models/Enums/UserDefaultsKey.swift
@@ -0,0 +1,10 @@
+//
+//  UserDefaultKeys.swift
+//  Oigetit_Replicate
+//
+//  Created by M Zaidaan Nugroho on 15/03/26.
+//
+
+enum UserDefaultsKey {
+    static let isShowHomeModal: String = "com.oigetit-replicate.isShow-home-modal"
+}
